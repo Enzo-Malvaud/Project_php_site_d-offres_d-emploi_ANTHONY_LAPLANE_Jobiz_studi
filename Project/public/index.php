c68a762054b3b0d5d05b71499005726921eacd12
Enzo-Malvaud <enzo.malvaud@gmail.com>
2025-11-10 17:09:01 +0100

    first commit

---
+++ Project/public/index.php
@@ -0,0 +1,13 @@
+<?php
+// Charger l'autoload
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// On définit un constante pour avoir le chemin racine de l'app
+define("APP_ROOT", dirname(__DIR__));
+
+
+
+use App\Controller\PageController;
+
+$pageController = new PageController();
+$pageController->home();
