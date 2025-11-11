b2856ae7e39576c0c89d3d0725bd0e75e4fa2bd1
Enzo-Malvaud <enzo.malvaud@gmail.com>
2025-11-11 20:04:03 +0100

    live 4

--- Project/public/index.php
+++ Project/public/index.php
@@ -4,11 +4,18 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 // On définit un constante pour avoir le chemin racine de l'app
 define("APP_ROOT", dirname(__DIR__));
+define("APP_ENV", ".env.local");
 
 use App\Routing\Router;
 
 $router = new Router();
 $router->handleResquest($_SERVER["REQUEST_URI"]);
+
+
+use App\Db\Mysql;
+
+$mysql = Mysql::getInstance();
+
 
 
 /*use App\Controller\PageController;
--- Project/src/Controller/PageController.php
+++ Project/src/Controller/PageController.php
@@ -2,23 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+
 class PageController extends Controller
 {
     public function home(): void
     {
         $gretting = "Hello";
         $name = "John";
-        $this->render("page/home", ["grettings" => $gretting, "name" => $name,]);
+        
+        $categoryRepository = new CategoryRepository();
+
+        $categories = $categoryRepository->findAll();
+
+        $this->render("page/home", [
+            "grettings" => $gretting, 
+            "name" => $name,
+            "categories" => $categories,
+        ]);
     }
     public function about(): void
     {
         $this->render("page/about");
     }
 
-    public function test(): void
-    {
-        $this->render("page/test");
-    }
+
 
 
 }
